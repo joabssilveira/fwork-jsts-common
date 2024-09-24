0a3103051c449fb2d7c3b1b2d78b85a79985e175
joabssilveira <joabssilveira@gmail.com>
2024-09-24 17:42:54 -0300

    fix:remove idbgetresult dependency

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,20 @@
-import { IDbGetResult } from 'fwork.db.typescript/dbClient'
-
 export interface IApiResult<T> {
   data?: T
   success?: boolean
   msg?: string
 }
 
-export interface IApiGetResult<T> extends IDbGetResult<T> {
-  
+export interface IApiGetResult<T> {
+  payload?: T,
+  pagination?: {
+    skip?: number,
+    limit?: number,
+    count?: number,
+    pageCount?: number,
+    currentPage?: number,
+  }
 }
 
 export interface IApiGetResultExt<T> extends IApiResult<IApiGetResult<T>> {
-  
+
 }
