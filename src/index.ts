0857cdf70664400383018d1d27504c224601608c
joabssilveira <joabssilveira@gmail.com>
2025-02-12 12:34:55 -0300

    fix:export Where type on index

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ import moment from 'moment';
 import { v4 as uuid } from 'uuid';
 import {
   ApiRequestDeleteOptions, ApiRequestGetOptions, ApiRequestPostOptions, ApiRequestPutOptions, ApiResponseDeleteData,
-  ApiResponseGetData, ApiResponseGetListData, ApiResponsePostData, ApiResponsePutData,
+  ApiResponseGetData, ApiResponseGetListData, ApiResponsePostData, ApiResponsePutData, 
 } from './api'
-import { ApiClientGetOptions, ApiClientUtils, BaseApiClient } from "./apiClient";
+import { Where } from './api/query'
+import { ApiClientGetOptions, ApiClientUtils, BaseApiClient, IApiClientResult } from "./apiClient";
+
+export { 
+  ApiRequestDeleteOptions, ApiRequestGetOptions, ApiRequestPostOptions, ApiRequestPutOptions, ApiResponseDeleteData,
+  ApiResponseGetData, ApiResponseGetListData, ApiResponsePostData, ApiResponsePutData,
+  Where,
+  ApiClientGetOptions, ApiClientUtils, BaseApiClient, IApiClientResult,
+};
 
 export const showDebugLog = false
 
@@ -564,9 +572,3 @@ export class ConsoleLogUtils {
     return process
   }
 }
-
-export { 
-  ApiRequestDeleteOptions, ApiRequestGetOptions, ApiRequestPostOptions, ApiRequestPutOptions, ApiResponseDeleteData,
-  ApiResponseGetData, ApiResponseGetListData, ApiResponsePostData, ApiResponsePutData,
-  ApiClientGetOptions, ApiClientUtils, BaseApiClient, 
-};
